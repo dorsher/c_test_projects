51224e016f597ddee40253c773e9f68735b69f44
dorsher <106176453+dorsher@users.noreply.github.com>
2023-04-23 14:57:17 +0300

    deleted

--- base32.h
+++ base32.h
@@ -1,18 +1,1 @@
-/**
- * @file base32.h
- * @author Doron Sher
- * @brief header file, convert to base 32
- * @version 0.1
- * @date 2022-08-17
- */
-
-#include "globals.h"
-#include <stdio.h>
-
-/**
- * Convert to base 32 as was requested in the "imaginary" assembly language
- *
- * @param value - decimal value
- * @param fp - file path to write to
- */
-void write_as_base_32(cell_value_type value, FILE *fp);
+deleted
